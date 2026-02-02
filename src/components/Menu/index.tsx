90b4ad5192071d6efddb45d3ee6a31a74c17173c
daniel.carmo <daniel.carmo2008@gmail.com>
2026-02-02 16:42:02 -0400

    feat: add dark and light theme change

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,21 +1,55 @@
-import { History, Home, Settings, Sun } from 'lucide-react';
+import { History, Home, Moon, Settings, Sun } from 'lucide-react';
 import styles from './styles.module.css';
 import { ButtonIcon } from '../ButtonIcon';
+import { useEffect, useState } from 'react';
+
+type AvailableThemes = 'dark' | 'light';
 
 export function Menu() {
+	const [theme, setTheme] = useState<AvailableThemes>(() => {
+		const localStorageTheme = localStorage.getItem('theme') as AvailableThemes;
+
+		return localStorageTheme || 'dark';
+	});
+
+	const themeIcons = {
+		dark: <Sun />,
+		light: <Moon />,
+	};
+
+	const handleThemeChange = (
+		event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+	) => {
+		event.preventDefault();
+
+		setTheme((prevTheme) => {
+			const nextTheme = prevTheme === 'dark' ? 'light' : 'dark';
+			localStorage.setItem('theme', nextTheme);
+			return nextTheme;
+		});
+	};
+
+	useEffect(() => {
+		document.documentElement.setAttribute('data-theme', theme);
+	}, [theme]);
+
 	return (
 		<nav className={styles.menu}>
-			<ButtonIcon>
+			<ButtonIcon title="Ir para Home" aria-label="Ir para Home">
 				<Home />
 			</ButtonIcon>
-			<ButtonIcon>
+			<ButtonIcon title="Histórico" aria-label="Histórico">
 				<History />
 			</ButtonIcon>
-			<ButtonIcon>
+			<ButtonIcon title="Configurações" aria-label="Configurações">
 				<Settings />
 			</ButtonIcon>
-			<ButtonIcon>
-				<Sun />
+			<ButtonIcon
+				onClick={handleThemeChange}
+				title="Mudar Tema"
+				aria-label="Mudar Tema"
+			>
+				{themeIcons[theme]}
 			</ButtonIcon>
 		</nav>
 	);
