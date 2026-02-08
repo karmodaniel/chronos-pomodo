a82e3ec05e75ffdc2bf53d273b565afddfc28498
daniel.carmo <daniel.carmo2008@gmail.com>
2026-02-08 19:29:45 -0400

    feat: reorder history table on th click

--- src/components/History/index.tsx
+++ src/components/History/index.tsx
@@ -7,9 +7,34 @@ import { ButtonIcon } from '../ButtonIcon';
 import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
 import { formatDate } from '../utils/formatDate';
 import { getTaskStatus } from '../utils/getTaskStatus';
+import { sortTasks, type SortTasksOptions } from '../utils/sortTasks';
+import { useState } from 'react';
 
 export function History() {
 	const { state } = useTaskContext();
+	const [sortTasksOptions, setSortTaskOptions] = useState<SortTasksOptions>(
+		() => {
+			return {
+				tasks: sortTasks({ tasks: state.tasks }),
+				field: 'startDate',
+				direction: 'desc',
+			};
+		}
+	);
+
+	function handleSortTasks({ field }: Pick<SortTasksOptions, 'field'>) {
+		const newDirection = sortTasksOptions.direction === 'desc' ? 'asc' : 'desc';
+
+		setSortTaskOptions({
+			tasks: sortTasks({
+				direction: newDirection,
+				tasks: sortTasksOptions.tasks,
+				field,
+			}),
+			direction: newDirection,
+			field,
+		});
+	}
 
 	return (
 		<MainTemplate>
@@ -34,16 +59,31 @@ export function History() {
 					<table>
 						<thead>
 							<tr>
-								<th>Tarefa</th>
-								<th>Duração</th>
-								<th>Data</th>
+								<th
+									onClick={() => handleSortTasks({ field: 'name' })}
+									className={styles.thSort}
+								>
+									Tarefa ↕
+								</th>
+								<th
+									onClick={() => handleSortTasks({ field: 'duration' })}
+									className={styles.thSort}
+								>
+									Duração ↕
+								</th>
+								<th
+									onClick={() => handleSortTasks({ field: 'startDate' })}
+									className={styles.thSort}
+								>
+									Data ↕
+								</th>
 								<th>Status</th>
 								<th>Tipo</th>
 							</tr>
 						</thead>
 
 						<tbody>
-							{state.tasks.map((task) => {
+							{sortTasksOptions.tasks.map((task) => {
 								const taskTypeMap = {
 									workTime: 'Foco',
 									shortBreakTime: 'Descanso curto',
